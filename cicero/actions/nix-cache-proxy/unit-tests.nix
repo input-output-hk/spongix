6e4fea2d76e46c1931ce17d7d9b32c24a26b30f6
Michael Fellinger <michael.fellinger@iohk.io>
2022-02-17 16:05:46 +0100

    proper ci job

--- cicero/actions/nix-cache-proxy/unit-tests.nix
+++ cicero/actions/nix-cache-proxy/unit-tests.nix
@@ -2,21 +2,43 @@
 let startOf = of: of.value."${name}".start;
 in {
   inputs.start = ''
-    "${name}": start: clone_url: string
+    "${name}": start: {
+      clone_url: string
+      sha: string
+      statuses_url?: string
+    }
   '';
 
   output = { start }: {
     success."${name}" = {
       ok = true;
-      inherit (startOf start) clone_url;
+      inherit (start.value."${name}".start) clone_url;
     };
   };
 
   job = { start }:
-    std.chain args [
+    let facts = start.value."cicero/ci".start;
+    in std.chain args [
       actionLib.simpleJob
+
+      {
+        resources.memory = 1024 * 3;
+        config.packages = std.data-merge.append [
+          "github:input-output-hk/nix-cache-proxy/${cfg.sha}#devShell.x86_64-linux"
+        ];
+      }
+
+      (lib.optionalAttrs (cfg ? statuses_url)
+        (std.github.reportStatus cfg.statuses_url))
+
+      (std.git.clone cfg)
+
       (std.script "bash" ''
-        echo hi
+        set -ex
+        lint
+        ${lib.escapeShellArgs next}
       '')
+
+      std.nix.build
     ];
 }

cc1e68b97647f5ce8e5ed63048486bf556b157d2
Michael Fellinger <michael.fellinger@iohk.io>
2022-02-17 16:22:54 +0100

    fix the nix build

--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 { buildGoModule, inclusive, rev }:
 buildGoModule rec {
   pname = "nix-cache-proxy";
-  version = "2022.02.16.001";
+  version = "2022.02.17.001";
   vendorSha256 = "sha256-wUfj/ba+7RLy8xPsc9ClaeA0oh85DZdsipQEErF7lHg=";
 
   src = inclusive ./. [
@@ -20,14 +20,8 @@ buildGoModule rec {
     ./router_test.go
   ];
 
-  CGO_ENABLED = "0";
-  GOOS = "linux";
+  CGO_ENABLED = "1";
 
-  ldflags = [
-    "-s"
-    "-w"
-    "-extldflags"
-    "-static"
-    "-X main.buildVersion=${version} -X main.buildCommit=${rev}"
-  ];
+  ldflags =
+    [ "-s" "-w" "-X main.buildVersion=${version} -X main.buildCommit=${rev}" ];
 }
